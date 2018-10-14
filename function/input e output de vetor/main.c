e22fe4127b640e163453cb7cc6d18697194fb804
cindRoberta <32280512+cindRoberta@users.noreply.github.com>
2018-10-14 19:45:23 -0300

    Create main.c

---
+++ function/input e output de vetor/main.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void entradaVetor(int *pVetor, int x)
+{
+  for(int i=0; i<x; i++)
+    scanf("%i", &pVetor[i]);
+}
+
+void mostrarVetor(int *pVetor, int x)
+{
+  for(int i=0; i<x; i++)
+    printf("%i, ", pVetor[i]);
+}
+
+int main()
+{
+  int tam= 3, *vetor= malloc(0);
+
+  entradaVetor(vetor,tam);
+  printf("\n");
+  mostrarVetor(vetor,tam);
+}
