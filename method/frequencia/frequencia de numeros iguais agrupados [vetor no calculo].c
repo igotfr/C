eba1d2927ca0de3c024278aa40d9a7ffd9168c8e
cindRoberta <32280512+cindRoberta@users.noreply.github.com>
2018-11-16 18:58:43 -0200

    Update frequencia de numeros iguais agrupados [vetor no calculo].c

--- method/frequencia/frequencia de numeros iguais agrupados [vetor no calculo].c
+++ method/frequencia/frequencia de numeros iguais agrupados [vetor no calculo].c
@@ -2,7 +2,7 @@
 
 int main()
 {
-	int n= 14, vetor[]= {1,1,1,1,3,3,4,4,4,5,5,6,6,9}, iVetor= 0, jVetor= iVetor+1, freq[n], vetorExclusivos[n], iVExclusivos= 0;
+	int n= 14, vetor[]= {1,1,1,1,3,3,4,4,4,5,5,6,6,9}, iVetor= 0, jVetor= iVetor+1, vetorExclusivos[n], freq[n], iVExclusivos= 0;
 
 	while( iVetor<14 )
 	{
