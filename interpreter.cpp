9583b0fbaa2327c2e3d249e299902d3f2dd1d52d
cindRoberta <32280512+cindRoberta@users.noreply.github.com>
2021-10-05 20:01:54 -0300

    Update interpreter.cpp

--- interpreter.cpp
+++ interpreter.cpp
@@ -1,3 +1,16 @@
+// sudo dnf install llvm
+
+#if 0
+CXX=clang++
+CXXFLAGS="-O2 -Wall -Werror -std=c++17"
+CXXARGS="-xc++ -emit-llvm -c -o -"
+CXXCMD="$CXX $CXXFLAGS $CXXARGS $0"
+LLICMD="lli -force-interpreter -fake-argv0=$0 -"
+$CXXCMD | $LLICMD "$@" ; exit $?
+#endif
+
+// allow to use stdin
+
 #if 0
 CXX=clang++
 CXXFLAGS="-O2 -Wall -Werror -std=c++17"
@@ -6,5 +19,3 @@ CXXCMD="$CXX $CXXFLAGS $CXXARGS $0"
 LLICMD="lli -force-interpreter -fake-argv0=$0"
 exec $LLICMD <($CXXCMD) "$@"
 #endif
-  
-// sudo dnf install llvm
