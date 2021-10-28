21dc87ecb49941f84ff4cfefe675fb5c91760a2e
cindRoberta <32280512+cindRoberta@users.noreply.github.com>
2021-10-28 18:59:54 -0300

    Create macros.c

---
+++ structure/macros.c
@@ -0,0 +1,8 @@
+#define u8 __uint8_t
+#define u16 __uint16_t
+#define u32 __uint32_t
+#define u64 __uint64_t
+#define i8 __int8_t
+#define i16 __int16_t
+#define i32 __int32_t
+#define i64 __int64_t
