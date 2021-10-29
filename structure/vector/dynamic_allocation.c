da70582933acbe754a7e2b9650bdd25c8ebb8de0
cindRoberta <32280512+cindRoberta@users.noreply.github.com>
2021-10-28 21:40:09 -0300

    Create dynamic_allocation.c

---
+++ structure/vector/dynamic_allocation.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void entradaVetor(int* pVetor, int size) {
+  for (int i = 0; i <= size; i++)
+    scanf("%i", &pVetor[i]);
+}
+
+void mostrarVetor(int* pVetor, int size) {
+  for (int i = 0; i <= size; i++)
+    printf("Index %i: %i\n", i, pVetor[i]);
+}
+
+int main(void) {
+  int *vetor = malloc(0), tam = 2;
+
+  entradaVetor(vetor, tam);
+  printf("\n");
+  mostrarVetor(vetor, tam);
+
+  free(vetor);
+
+  return 0;
+}
