d64c97b93631dd00f437c2e6a627875e7ba5246e
cindRoberta <32280512+cindRoberta@users.noreply.github.com>
2021-10-28 22:01:15 -0300

    Create vector_frequency.c

---
+++ algorithm/frequency/vector_frequency.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int main() {
+  int achou, n = 7, i, j, k = -1, freq[n];
+  float vetor[] = {6,6,8,6,8,9,4}, vetorExcusivos[n];
+
+  for (i = 0; i < n; i++) {
+    achou = 0;
+
+    for (j = 0; j <= k; j++)
+      if (vetor[i] == vetorExcusivos[j]) {
+        achou = 1;
+        freq[j]++;
+        break;
+      }
+
+    if (!achou) {
+      k++;
+      vetorExcusivos[k] = vetor[i];
+      freq[k] = 1;
+    }
+  }
+
+  printf("   Num   Freq\n");
+  for (j = 0; j <= k; j++) {
+    printf("%6.1lf", vetorExcusivos[j]);
+    printf("%7d\n", freq[j]);
+  }
+
+  return 0;
+}
