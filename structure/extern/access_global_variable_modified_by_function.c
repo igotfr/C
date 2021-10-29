31aa9dcbbbad769df01868a131eeb9fb7bf567fa
cindRoberta <32280512+cindRoberta@users.noreply.github.com>
2021-10-28 21:21:32 -0300

    Create access_global_variable_modified_by_function.c

---
+++ structure/extern/access_global_variable_modified_by_function.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int var = 1;
+
+void funcao() {
+  extern int var;
+  var = 2;
+}
+
+int main() {
+  printf("Valor da variável antes da função ser chamada: %i\n", var);
+
+  funcao();
+  printf("Valor da variável após a função ser chamada: %i", var);
+}
