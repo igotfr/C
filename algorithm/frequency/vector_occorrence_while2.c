cd85c89630f7b63051282a63684d33412a4687d6
cindRoberta <32280512+cindRoberta@users.noreply.github.com>
2021-10-28 23:25:20 -0300

    Create vector_occorrence_while2.c

---
+++ algorithm/frequency/vector_occorrence_while2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main() {
+  int n = 14, vetor[] = {1,1,1,1,3,3,4,4,4,5,5,6,6,9}, iVetor = 0, jVetor = iVetor + 1, vetorExclusivos[n], freq[n], iVExclusivos = 0;
+
+  while (iVetor < 14) {
+    vetorExclusivos[iVExclusivos] = vetor[iVetor];
+    freq[iVExclusivos] = 1;
+
+    while (vetor[iVetor] == vetor[jVetor]) {
+      freq[iVExclusivos]++;
+      iVetor++, jVetor++;
+    }
+
+    iVExclusivos++;
+    iVetor++, jVetor++;
+  }
+
+  for (int x = 0; x < iVExclusivos; x++)
+    printf("numero: %i\tocorrencias: %i\n", vetorExclusivos[x], freq[x]);
+
+  return 0;
+}
